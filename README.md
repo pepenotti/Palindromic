ecb2e561b50367ad5de5c73c03093cbeb4f9530d
pepenotti <antonio.franchinotti@southworks.com>
2025-03-13 17:22:00 -0300

    Add readme

---
+++ README.md
@@ -0,0 +1,90 @@
+# Palindromic
+
+This repo was created as a test for a job interview.
+
+The objective was to develop an application that finds the largest palindromic number between two given numbers (exclusive).
+
+## How to run
+
+You can have a quick check on the functionality by visiting [this link](https://pepenotti.github.io/Palindromic/).
+
+If you want to run the application locally, you can follow the instructions below and run it as:
+- [Running as Website and Azure Function](#running-as-website-and-azure-function)
+- [Running as Console Application](#running-as-console-application)
+
+
+### Running as Website and Azure Function
+
+#### Prerequisites
+
+- [.NET 8 SDK](https://dotnet.microsoft.com/download)
+- [Node.js](https://nodejs.org/en/download/)
+- [Azure Functions Core Tools](https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local)
+
+#### Running the Azure Function
+
+1. Open a terminal window.
+2. Navigate to the directory containing your Azure Function project:
+
+    ```sh
+    cd .\src\Palindromic.Functions
+    ```
+
+3. Start the Azure Function locally using the Azure Functions Core Tools:
+
+    ```sh
+    func start
+    ```
+
+#### Running the Web Application
+
+1. Open another terminal window or tab.
+2. Navigate to the directory containing your web application project:
+
+    ```sh
+    cd .\src\palindromic.web
+    ```
+
+3. Install the dependencies if you haven't already:
+
+    ```sh
+    npm install
+    ```
+
+4. Start the web application:
+
+    ```sh
+    npm run dev -- --host
+    ```
+
+### Configuration
+
+Ensure that your web application is configured to communicate with the locally running Azure Function. You can set the `VITE_AZURE_FUNCTION_URL` environment variable to point to the local URL of the Azure Function, typically `http://localhost:7071/api`.
+
+You can set this environment variable in a [.env](./src/palindromic.web/.env) file in your web application directory:
+
+```properties
+VITE_AZURE_FUNCTION_URL=http://localhost:7071/api
+```
+
+### Running as Console Application
+
+#### Prerequisites
+
+- [.NET 8 SDK](https://dotnet.microsoft.com/download)
+- [Azure Functions Core Tools](https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local)
+
+
+#### Running the Console Application
+
+1. Open a terminal window.
+2. Navigate to the directory containing your console application project:
+
+    ```sh
+    cd .\src\Palindromic.Console
+    ```
+3. Run the console application:
+
+    ```sh
+    dotnet run {numberA} {numberB}
+    ```
--- src/palindromic.web/src/App.jsx
+++ src/palindromic.web/src/App.jsx
@@ -4,68 +4,69 @@ import './App.css'
 
 
 function App() {
-  const [input1, setInput1] = useState('')
-  const [input2, setInput2] = useState('')
-  const [result, setResult] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+	const [input1, setInput1] = useState('')
+	const [input2, setInput2] = useState('')
+	const [result, setResult] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setLoading(true)
-    setError(null)
-    try {
-      const response = await fetch(`${import.meta.env.VITE_AZURE_FUNCTION_URL}/api/longestbetween/${input1}/${input2}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-      const data = await response.json()
-      setResult(data)
-    } catch (error) {
-      setError('Failed to fetch data. Please try again.')
-      console.error('Error:', error)
-    } finally {
-      setLoading(false)
-    }
-  }
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		setLoading(true)
+		setError(null)
+		try {
+			const response = await fetch(`${import.meta.env.VITE_AZURE_FUNCTION_URL}/api/longestbetween/${input1}/${input2}`, {
+				method: 'GET',
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			if (!response.ok) {
+				throw new Error('Network response was not ok')
+			}
+			const data = await response.json()
+			setResult(data)
+		} catch (error) {
+			setError('Failed to fetch data. Please try again.')
+			console.error('Error:', error)
+		} finally {
+			setLoading(false)
+		}
+	}
 
-  return (
-    <div className="container">
-      <h1>Find the highest palindrome between</h1>
-      <form onSubmit={handleSubmit} className="form">
-        <input
-          type="number"
-          value={input1}
-          onChange={(e) => setInput1(e.target.value)}
-          placeholder="From"
-          className="input"
-          min="-2147483648"
-          max="2147483647"
-        />
-        <input
-          type="number"
-          value={input2}
-          onChange={(e) => setInput2(e.target.value)}
-          placeholder="To"
-          className="input"
-          min="-2147483648"
-          max="2147483647"
-        />
-        <br />
-        <button type="submit" className="button" disabled={loading}>
-          {loading ? 'Loading...' : 'Submit'}
-        </button>
-      </form>
-      {loading && <p className="loader">Loading...</p>}
-      {error && <p className="error">{error}</p>}
-      {result && <p className="result">Result: {JSON.stringify(result)}</p>}
-    </div>
-  )
+	return (
+		<div className="container">
+			<h1>Find the highest palindrome between</h1>
+			<form onSubmit={handleSubmit} className="form">
+				<input
+					type="number"
+					value={input1}
+					onChange={(e) => setInput1(e.target.value)}
+					placeholder="From"
+					className="input"
+					min="-2147483648"
+					max="2147483647"
+				/>
+				<input
+					type="number"
+					value={input2}
+					onChange={(e) => setInput2(e.target.value)}
+					placeholder="To"
+					className="input"
+					min="-2147483648"
+					max="2147483647"
+				/>
+				<br />
+				<button type="submit" className="button" disabled={loading}>
+					{loading ? 'Loading...' : 'Submit'}
+				</button>
+			</form>
+			{loading && <p className="loader">Loading...</p>}
+			{error && <p className="error">{error}</p>}
+			{result && <p className="result">Result: {JSON.stringify(result)}</p>}
+		</div>
+	)
 }
 
 export default App
